82cfa4fe2ea1fe87e3a515655a1efa4cc8aa36d3
FBNikita <funnybunnynikita@mail.ru>
2018-11-16 20:06:56 +0300

    upd
    Добавил файл для добровольных переводчиков.

--- modinfo.lua
+++ modinfo.lua
@@ -1,15 +1,15 @@
 
 name = "Russian Language Pack"
-version = "4.1"
-desc = {en = "Russian localization of the game compiled from the user-suggested translations. The best translations are chosen via user voting. \n\nVersion: "..version,
-		ru = "\nРусификация игры, собранная на основе\nголосований за переводы реплик.\nВерсия: "..version}
+version = "0.1"
+desc = {en = "Temporary machine translation of the Hamlet addon.  Soure code was taken from the Russian Language Pack. Special thanks to Some1 and Alevastor!\n\nVersion: "..version,
+		ru = "\nВременный машинный перевод дополнения Hamlet. Код для базы был взят из мода Russian Language Pack.Огромное спасибо Some1'у и Alevastor'у за проделанную работу!\n\nВерсия: "..version}
 description = desc[language] or desc["en"]
-author = "Some1"
-forumthread = "/files/file/879-russian-language-pack/"
+author = "??, Popugayman"
 api_version = 6
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
-priority = 1
+priority = .9999999999991 --Сразу после старого русификатора
 dont_starve_compatible = true
 reign_of_giants_compatible = true
 shipwrecked_compatible = true
+hamlet_compatible = true
